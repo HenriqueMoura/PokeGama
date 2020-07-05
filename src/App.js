de24889d28415612e4104b1cf9c6cffba6581637
Henrique <henrique.almeida@ecommercebrasil.com.br>
2020-07-05 03:00:04 -0300

    Fazendo consulta API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import axios from 'axios'
+
 
 function App() {
+
+  function getPokemon(){
+    axios.get('https://pokeapi.co/api/v2/pokemon')
+    .then(Response => console.log(Response.data.results))
+  }
+  getPokemon(); 
+ 
   return (
     <div className="App">
-      <p>teste</p>
     </div>
   );
 }
